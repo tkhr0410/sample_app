c7898c1f5f729c1460ee2feb4bf175d7a78078ad
日置貴大 <tkhr4h@gmail.com>
2018-03-30 04:39:19 +0000

    ユーザーフォロー機能の追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,39 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, :except=>[:show]
+  before_action :admin_user, only: :destroy
+  
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.paginate(page: params[:page], per_page: 5)
   end
   
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page], per_page: 5)
+  end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "ユーザーを削除しました。"
+    redirect_to users_url
+  end
+  
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.followed_users.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
+  
+  private
+  
+  def admin_user
+    redirect_to(root_path) unless current_user.admin? 
   end
 end
